35621df6c4e2beacb9a1687075135edb41d30776
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-08-06 17:20:48 +0200

    update

--- CafePointOfSale.Data/Repositories/ItemRepository.cs
+++ CafePointOfSale.Data/Repositories/ItemRepository.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CafePointOfSale.Data.Repositories
+﻿namespace CafePointOfSale.Data.Repositories
 {
-    internal class ItemRepository
+    public class ItemRepository
     {
     }
 }
---
+++ CafePointOfSale.Data/Repositories/TimeOfDayRepository.cs
@@ -0,0 +1,12 @@
+﻿using CafePointOfSale.Core.Interfaces.Repositories;
+
+namespace CafePointOfSale.Data.Repositories
+{
+    public class TimeOfDayRepository : ITimeOfDayRepository
+    {
+        public int GetTimeOfDay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
