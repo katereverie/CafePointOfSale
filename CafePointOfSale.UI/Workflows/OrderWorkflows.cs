35855a7e94df92d2f62570988c7b5237945cc2e9
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-08-08 14:44:30 +0200

    update: CreateOrder, AddItemsToOrder, ViewOpenOrders complete functional

--- CafePointOfSale.UI/Workflows/OrderWorkflows.cs
+++ CafePointOfSale.UI/Workflows/OrderWorkflows.cs
@@ -29,7 +29,7 @@ namespace CafePointOfSale.UI.Workflows
             IO.PrintActiveServers(activeServers);
 
             int serverID = IO.GetServerID(activeServers, "Enter the ID of an available server: ");
-            CafeOrder newOrder = new CafeOrder { ServerID = serverID, OrderDate = DateTime.Now };
+            CafeOrder newOrder = new CafeOrder { ServerID = serverID, OrderDate = DateTime.Now, PaymentTypeID = default};
             var coResult = service.CreateOrder(newOrder);
 
             if (coResult.Ok)
@@ -46,24 +46,6 @@ namespace CafePointOfSale.UI.Workflows
 
         public static void AddItemsToOrder(IOrderService service)
         {
-            /*
-             * 1. Display open orders if any (x)
-             * 2. Get orderID from user via IO (x)
-             * 3. Initiate a List of order items
-             * 4. Start a loop 
-             *    a. Display available categories
-             *    b. User may choose to exit (enter 0) loop
-             *    c. Get categoryID from user via IO
-             *    d. Get timeOfDayID from TimeOfDayRepo
-             *    d. Display available items based on chosen categoryID
-             *    e. Get itemID and quantity from user via IO
-             *    f. Initiate OrderItem based on e
-             *    g. Add OrderItem to List
-             *    h. Prompt user for adding more or quit adding 
-             * 5. Invoke AddItemsToOrder
-             * 6. Display result + order summary 
-             */
-
             Console.Clear();
 
             var gooResult = service.GetOpenOrders();
@@ -83,6 +65,7 @@ namespace CafePointOfSale.UI.Workflows
             var openOrders = gooResult.Data;
             IO.PrintOpenOrders(openOrders);
             int orderID = IO.GetOrderID(openOrders, "Enter the ID of an open order: ");
+            CafeOrder order = openOrders.Single(op => op.OrderID == orderID);
 
             List<OrderItem> totalItems = new List<OrderItem>();
             bool hasMoreItemsToAdd = true;
@@ -113,37 +96,113 @@ namespace CafePointOfSale.UI.Workflows
                         Console.WriteLine(gaiResult.Message);
                         break;
                     }
+                    else if (!gaiResult.Data.Any())
+                    {
+                        Console.WriteLine("Currently, there is no available item.");
+                        break;
+                    }
 
-                    int itemID = IO.GetItemID(gaiResult.Data, "Enter the ID of an available item: ");
+                    var availableItems = gaiResult.Data;
+                    IO.PrintAvailableItems(availableItems);
+                    int itemID = IO.GetItemID(availableItems, "Enter the ID of an available item: ");
                     byte quantity = IO.GetQuantity("Enter Quantity: ");
                     if (quantity == 0)
                     {
                         continue;
                     }
 
+                    var itemToAdd = availableItems.Single(i => i.ItemID == itemID);
+
                     OrderItem newOrderItem = new OrderItem
                     {
                         OrderID = orderID,
-                        Quantity = quantity
+                        Quantity = quantity,
+                        ExtendedPrice = itemToAdd.ItemPrice.Price * quantity,
+                        ItemPrice = itemToAdd.ItemPrice
                     };
 
-                    totalItems.Add(newOrderItem);
-                    service.GetOrderSummary(totalItems);
-                    IO.PrintOrderSummary(totalItems);
+                    order.OrderItems.Add(newOrderItem);
+                    order = service.CalculateSubtotalAndTax(order);
+                    IO.PrintOrderSummary(order);
 
                     hasMoreItemsToAdd = IO.HasMoreItemsToAdd();
-                    
-                } while (true);
+                    IO.AnyKey();
+                    Console.Clear();
+
+                } while (hasMoreItemsToAdd);
+
             }
+
+            var atoResult = service.ProcessOrder(order);
+            if (atoResult.Ok)
+            {
+                Console.WriteLine("Order successfully processed.");
+            }
+            else
+            {
+                Console.WriteLine(atoResult.Message);
+            }
+
+            IO.AnyKey();
         }
 
         public static void ViewOpenOrders(IOrderService service)
         {
+            Console.Clear();
 
+            var gooResult = service.GetOpenOrders();
+
+            if (!gooResult.Ok)
+            {
+                Console.WriteLine(gooResult.Message);
+                IO.AnyKey();
+                return;
+            }
+            else if (!gooResult.Data.Any())
+            {
+                Console.WriteLine("Currently, there is no open order.");
+                IO.AnyKey();
+                return;
+            }
+
+            var openOrders = gooResult.Data;
+            IO.PrintOpenOrders(openOrders);
+
+            do
+            {
+                int choice = IO.GetInteger("Would you like to view details of a specific order?\n1. Yes\n0. No\nEnter choice: ");
+                if (choice == 0)
+                {
+                    IO.AnyKey();
+                    return;
+                }
+                else if (choice == 1)
+                {
+                    int orderID = IO.GetOrderID(openOrders, "Enter the ID of the order to view its details: ");
+                    var order = openOrders.Find(oo => oo.OrderID == orderID);
+                    IO.PrintOrderDetails(order);
+                    break;
+                }
+
+                Console.WriteLine("Invalid choice.");
+            } while (true);
+
+            IO.AnyKey();
         }
 
         public static void CancelOrder(IOrderService service)
         {
+            /*
+             * 1. Get open orders via service
+             * 2. Display open orders if any
+             * 3. Get orderID from user via IO
+             * 4. Process delete request via service
+             * 5. Display request result
+             */
+            Console.Clear();
+
+
+
 
         }
 
