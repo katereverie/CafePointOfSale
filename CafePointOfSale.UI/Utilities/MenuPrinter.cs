9f2b4d49e987cedd8e5d853f8b7aa5b024e754fd
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-08-08 14:35:51 +0200

    update: add display methods

---
+++ CafePointOfSale.UI/Utilities/MenuPrinter.cs
@@ -0,0 +1,38 @@
+﻿namespace CafePointOfSale.UI.Utilities
+{
+    public static class MenuPrinter
+    {
+        public static void DisplayMainMenu()
+        {
+            Console.WriteLine("Main Menu\n");
+            Console.WriteLine("1. Manage Orders");
+            Console.WriteLine("2. Manage Payment");
+            Console.WriteLine("3. View Reports");
+            Console.WriteLine("0. Exit\n");
+        }
+
+        public static void DisplayOrderMenu()
+        {
+            Console.WriteLine("Order Menu\n");
+            Console.WriteLine("1. Create New Order");
+            Console.WriteLine("2. Add Items to Order");
+            Console.WriteLine("3. View Open Orders");
+            Console.WriteLine("4. Cancel Order");
+            Console.WriteLine("0. Go back\n");
+        }
+
+        public static void DisplayPaymentMenu()
+        {
+            Console.WriteLine("Payment Management\n");
+            Console.WriteLine("1. Process Payment");
+            Console.WriteLine("0. Go back\n");
+        }
+
+        public static void DisplayReportMenu()
+        {
+            Console.WriteLine("Report Options\n");
+            Console.WriteLine("1. Daily Sales Report");
+            Console.WriteLine("0. Go back\n");
+        }
+    }
+}
