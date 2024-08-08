6df8f641bda8f9b27d6f6f33bdabba0a5a5efefc
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-08-08 14:42:27 +0200

    Update: due to DbSet name change; fix GetActiveUsers where condition; fix GetOpenOrders to include related batch data

--- CafePointOfSale.Data/Repositories/OrderRepository.cs
+++ CafePointOfSale.Data/Repositories/OrderRepository.cs
@@ -15,48 +15,54 @@ namespace CafePointOfSale.Data.Repositories
 
         public int Add(CafeOrder order)
         {
-            _dbContext.CafeOrders.Add(order);
+            _dbContext.CafeOrder.Add(order);
             _dbContext.SaveChanges();
             return order.OrderID;
         }
 
         public void Delete(int orderID)
         {
-            var order = _dbContext.CafeOrders.FirstOrDefault(o => o.OrderID == orderID);
+            var order = _dbContext.CafeOrder.FirstOrDefault(o => o.OrderID == orderID);
 
             if (order != null)
             {
-                _dbContext.CafeOrders.Remove(order);
+                _dbContext.CafeOrder.Remove(order);
                 _dbContext.SaveChanges();
             }
         }
 
         public void Update(CafeOrder order)
         {
-            _dbContext.CafeOrders.Update(order);
+            _dbContext.CafeOrder.Update(order);
             _dbContext.SaveChanges();
         }
 
         public List<Server> GetActiveServers()
         {
-            return _dbContext.Servers.ToList();
+            return _dbContext.Server
+                .Where(s => s.TermDate == null || s.TermDate >= DateTime.Now)
+                .ToList();
         }
 
         public List<PaymentType> GetAllPaymentTypes()
         {
-            return _dbContext.PaymentTypes.ToList();
+            return _dbContext.PaymentType.ToList();
         }
 
         public List<CafeOrder> GetOpenOrders()
         {
-            return _dbContext.CafeOrders
-                .Where(o => o.PaymentTypeID == null)
+            return _dbContext.CafeOrder
+                .Include(co => co.Server)
+                .Include(co => co.OrderItems)
+                .ThenInclude(oi => oi.ItemPrice)
+                .ThenInclude(ip => ip.Item)
+                .Where(co => co.PaymentTypeID == null)
                 .ToList(); 
         }
 
         public CafeOrder GetOrderDetails(int orderID)
         {
-            return _dbContext.CafeOrders
+            return _dbContext.CafeOrder
                 .Include(o => o.OrderItems)
                 .Where(o => o.OrderID == orderID)
                 .First();
@@ -64,7 +70,7 @@ namespace CafePointOfSale.Data.Repositories
 
         public CafeOrder? GetByOrderID(int orderID)
         {
-            return _dbContext.CafeOrders
+            return _dbContext.CafeOrder
                 .Where(o => o.OrderID == orderID)
                 .FirstOrDefault();
         }
