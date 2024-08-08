ed96a33c32377e0e138951b967e01c6a5257bf7e
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-08-08 14:40:35 +0200

    fix: allow PaymentTypeID to be null

--- CafePointOfSale.Core/Entities/Tables/PaymentType.cs
+++ CafePointOfSale.Core/Entities/Tables/PaymentType.cs
@@ -2,7 +2,7 @@
 {
     public class PaymentType
     {
-        public int PaymentTypeID { get; set; }
+        public int? PaymentTypeID { get; set; }
         public string PaymentTypeName { get; set; }
 
         public List<CafeOrder> CafeOrders { get; set; }
