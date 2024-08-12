c3aa3fe984c3dd975b93c2d7da504facbdb80664
Katereverie <zhangjt.de@gmail.com>
2024-08-12 20:30:54 +0200

    update: complete getDailySalesReport()

--- CafePointOfSale.UI/Workflows/ReportWorkflows.cs
+++ CafePointOfSale.UI/Workflows/ReportWorkflows.cs
@@ -1,13 +1,14 @@
-﻿using CafePointOfSale.UI.Utilities;
+﻿using CafePointOfSale.Core.Interfaces.Services;
+using CafePointOfSale.UI.Utilities;
 
 namespace CafePointOfSale.UI.Workflows
 {
     public static class ReportWorkflows
     {
-        public static void GetDailySalesReport()
+        public static void GetDailySalesReport(IReportService service)
         {
             /* 
-            * 1. Get a Date from users via IO
+            * 1. (x) Get a Date from users via IO
             * 2. Get all orders and their items on given Date
             * 3. Display fetched orders and their itens via IO
             */
@@ -18,6 +19,18 @@ namespace CafePointOfSale.UI.Workflows
             // Each order may have several items
             // Decide what to display and how to display them
 
+            var gcobdResult = service.GetCafeOrdersByDate(date);
+
+            if (!gcobdResult.Ok || gcobdResult.Data == null || !gcobdResult.Data.Any()) 
+            {
+                Console.WriteLine(gcobdResult.Ok ? $"No Sales Report available on {date}" : gcobdResult.Message);
+            }
+            else 
+            {
+                IO.PrintDailySalesReport(gcobdResult.Data);
+            }
+
+            IO.AnyKey();
         }
     }
 }
