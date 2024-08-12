1d36cdccd0d0d48ce77605ddd51b28586a30ce93
Katereverie <zhangjt.de@gmail.com>
2024-08-12 20:18:08 +0200

    update: make ServerID and Server required

--- CafePointOfSale.Core/Entities/Tables/CafeOrder.cs
+++ CafePointOfSale.Core/Entities/Tables/CafeOrder.cs
@@ -6,7 +6,7 @@ namespace CafePointOfSale.Core.Entities.Tables
     {
         [Key]
         public int OrderID { get; set; }
-        public int ServerID { get; set; }
+        public required int ServerID { get; set; }
         public int? PaymentTypeID { get; set; }
         public DateTime? OrderDate { get; set; }
         public decimal? SubTotal { get; set; }
@@ -14,7 +14,7 @@ namespace CafePointOfSale.Core.Entities.Tables
         public decimal? Tip { get; set; }
         public decimal? AmountDue { get; set; }
 
-        public Server Server { get; set; }
-        public List<OrderItem> OrderItems { get; set; }
+        public required Server Server { get; set; }
+        public List<OrderItem>? OrderItems { get; set; }
     }
 }
