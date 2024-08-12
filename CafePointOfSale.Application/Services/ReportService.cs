e18dfa0442d0354406e848d5279b48e37cca46f2
Katereverie <zhangjt.de@gmail.com>
2024-08-12 19:43:37 +0200

    update

--- CafePointOfSale.Application/Services/ReportService.cs
+++ CafePointOfSale.Application/Services/ReportService.cs
@@ -1,9 +1,14 @@
+using CafePointOfSale.Core.Entities.DTOs;
+using CafePointOfSale.Core.Entities.Tables;
 using CafePointOfSale.Core.Interfaces.Services;
 
 namespace CafePointOfSale.Application.Services
 {
     public class ReportService : IReportService
     {
-        
+        public Result<List<CafeOrder>> GetCafeOrdersByDate(DateOnly date)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
