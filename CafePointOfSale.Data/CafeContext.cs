24ac3a4708e1065d1f03c95a6fccdee186d3d475
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-08-19 18:20:41 +0200

    update: remove unused dbset

--- CafePointOfSale.Data/CafeContext.cs
+++ CafePointOfSale.Data/CafeContext.cs
@@ -6,11 +6,8 @@ namespace CafePointOfSale.Data
     public class CafeContext : DbContext
     {
         private string _connectionString;
-
-        public DbSet<Category> Category { get; set; }
         public DbSet<Item> Item { get; set; }
         public DbSet<ItemPrice> ItemPrice { get; set; }
-        public DbSet<TimeOfDay> TimeOfDay { get; set; }
         public DbSet<OrderItem> OrderItem { get; set; }
         public DbSet<CafeOrder> CafeOrder { get; set; }
         public DbSet<PaymentType> PaymentType { get; set; }    
@@ -24,7 +21,9 @@ namespace CafePointOfSale.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer(_connectionString);
+            optionsBuilder
+                .UseSqlServer(_connectionString)
+                .EnableSensitiveDataLogging();
         }
     }
 }
