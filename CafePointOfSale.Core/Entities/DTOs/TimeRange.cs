a1febaae92791bc7f231395e012143c6498a888e
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-08-19 18:18:15 +0200

    update: remove unused dtos

--- CafePointOfSale.Core/Entities/DTOs/TimeRange.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CafePointOfSale.Core.Entities.DTOs
-{
-    public class TimeRange
-    {
-        public int TimeOfDayID { get; set; }
-        public TimeSpan StartTime { get; set; }
-        public TimeSpan EndTime { get; set; }
-    }
-}
