408bc2f5ec7fd611508b53a595a007f7857f311e
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-08-19 18:23:30 +0200

    update: remove todo comment

--- CafePointOfSale.UI/Workflows/ReportWorkflows.cs
+++ CafePointOfSale.UI/Workflows/ReportWorkflows.cs
@@ -7,11 +7,6 @@ namespace CafePointOfSale.UI.Workflows
     {
         public static void GetDailySalesReport(IReportService service)
         {
-            /* 
-            * 1. (x) Get a Date from users via IO
-            * 2. (x) Get all orders and their items on given Date
-            * 3. (x) Display fetched orders and their itens via IO
-            */
             Console.Clear();
             
             DateTime date = IO.GetDate("Enter a Date to view the sales report on the date: ");
